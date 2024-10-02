e3e488304fdd9e1ab7c20c329eabe5affd789ee8
xStumble <127465939+xStumble@users.noreply.github.com>
2024-10-02 21:47:10 +0700

    Add files via upload

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,11 +1,16 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         UserService userService = new UserServiceImpl();
 
         userService.createUsersTable();
@@ -16,9 +21,16 @@ public class Main {
         userService.saveUser("Name4", "LastName4", (byte) 38);
 
         userService.removeUserById(1);
-        userService.getAllUsers();
+
+        List<User> users = userService.getAllUsers();
+        for (User user : users) {
+            System.out.println(user);
+        }
+
         userService.cleanUsersTable();
         userService.dropUsersTable();
+
+        Util.closeConnection();
     }
 
 }
